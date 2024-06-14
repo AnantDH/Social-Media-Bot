50068779d9213bee55bbeb6a6220b74b2d2fb806
anantd <anantd@uw.edu>
2024-06-14 14:01:18 -0700

    checking config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py stores all secret API keys
+
+
+# redditscraper keys
+client_id = 'eJKpPMhgfo_wv5EPKi4yDg'
+client_secret = '7C7DHBJq6k8LdOs95-KEi-5st6ksmA'
+
+# TTS generator keys
+tts_key = "39b04335aa0283f65a8bb3e6f3b10682"
+
+# video generator keys
+video_generator_key = "4Kfz0TofWv4ZvjBwJMioC4vFJnjY5tLU33hQhLys"
