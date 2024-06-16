a72c5cc137eb945c75c6cbb2f005aa3c90382701
anantd <anantd@uw.edu>
2024-06-15 17:05:33 -0700

    changed gitignore

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py stores all secret API keys
+# google cloud keys are contained within credentials.json
+
+# test to see if gitignore works pART 2
+
+# redditscraper keys
+client_id = 'eJKpPMhgfo_wv5EPKi4yDg'
+client_secret = '7C7DHBJq6k8LdOs95-KEi-5st6ksmA'
+
+# TTS generator keys
+tts_key = "39b04335aa0283f65a8bb3e6f3b10682"
+
+# video generator keys
+video_generator_key = "4Kfz0TofWv4ZvjBwJMioC4vFJnjY5tLU33hQhLys"
